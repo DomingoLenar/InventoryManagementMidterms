844ffbfb9ec4da16a2b5a3af284c0e6090bf508c
Lestat <2233915@slu.edu.ph>
2024-03-21 00:47:38 +0800

    +project config

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -3,8 +3,7 @@ module com.example.inventorymanagement {
     requires javafx.fxml;
     requires com.google.gson;
 
-    opens com.example.inventorymanagement to javafx.fxml;
-    exports com.example.inventorymanagement;
+
     exports com.example.inventorymanagement.client.controller;
     opens com.example.inventorymanagement.client.controller to javafx.fxml;
 }
