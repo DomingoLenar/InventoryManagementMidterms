a4a1eeabac7947dea78f1abcc5770352d7dc964e
Lenar <2233894@slu.edu.ph>
2024-03-21 23:42:05 +0800

    + Refactoring & Module Info Changes

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -1,3 +1,4 @@
+
 module com.example.inventorymanagement {
     requires javafx.controls;
     requires javafx.fxml;
@@ -6,4 +7,8 @@ module com.example.inventorymanagement {
 
     exports com.example.inventorymanagement.client.controller;
     opens com.example.inventorymanagement.client.controller to javafx.fxml;
+    exports com.example.inventorymanagement.client.controller.welcome;
+    opens com.example.inventorymanagement.client.controller.welcome to javafx.fxml;
+    exports com.example.inventorymanagement.client.controller.login;
+    opens com.example.inventorymanagement.client.controller.login to javafx.fxml;
 }
