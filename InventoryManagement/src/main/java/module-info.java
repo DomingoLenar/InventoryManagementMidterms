6fe1dc65131607eea18767b6c8fd3b66801cef2d
Georcelle <2234664@slu.edu.ph>
2024-03-23 23:47:40 +0800

    added the needed exports and opens for the profile management panel, controller, and fxml

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -11,4 +11,6 @@ module com.example.inventorymanagement {
     opens com.example.inventorymanagement.client.controller.welcome to javafx.fxml;
     exports com.example.inventorymanagement.client.controller.login;
     opens com.example.inventorymanagement.client.controller.login to javafx.fxml;
+    exports com.example.inventorymanagement.client.controller.ProfileManagement;
+    opens com.example.inventorymanagement.client.controller.ProfileManagement to javafx.fxml;
 }
