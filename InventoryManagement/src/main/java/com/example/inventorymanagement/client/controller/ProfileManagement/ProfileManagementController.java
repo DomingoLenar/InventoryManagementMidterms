1895d6ae37fac31ed7abc8950cb3c1302688049d
Georcelle <2234664@slu.edu.ph>
2024-03-23 23:52:07 +0800

    created the profile management panels and created a controller class and panel class for it.

---
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/controller/ProfileManagement/ProfileManagementController.java
@@ -0,0 +1,50 @@
+package com.example.inventorymanagement.client.controller.ProfileManagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+public class ProfileManagementController {
+    @FXML
+    private BorderPane borderPaneProfileManagement;
+    @FXML
+    private ImageView personIconImage;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label changePasswordLabel;
+    @FXML
+    private TextField oldPasswordTextField;
+    @FXML
+    private TextField newPasswordTextField;
+    @FXML
+    private Button saveButton;
+
+    public BorderPane getBorderPaneProfileManagement() {
+        return borderPaneProfileManagement;
+    }
+    public ImageView getPersonIconImage(){
+        return personIconImage;
+    }
+
+    public Label getChangePasswordLabel() {
+        return changePasswordLabel;
+    }
+
+    public Label getEmailLabel() {
+        return emailLabel;
+    }
+
+    public Label getUsernameLabel() {
+        return usernameLabel;
+    }
+
+    public Button getSaveButton() {
+        return saveButton;
+    }
+}
---
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/controller/ProfileManagement/ProfileManagementPanel.java
@@ -0,0 +1,38 @@
+package com.example.inventorymanagement.client.controller.ProfileManagement;
+
+import com.example.inventorymanagement.client.controller.login.LoginPanel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ProfileManagementPanel extends Application {
+    public void start(Stage stage) throws IOException {
+
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginPanel.class.getResource("/com/example/inventorymanagement/client/view/profileManagement/profileManagement-view.fxml"));
+        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
+
+        if (inputStream != null) {
+            Image image = new Image(inputStream);
+            stage.getIcons().add(image);
+        } else {
+            System.err.println("Failed to load image: logo.png");
+        }
+
+        Scene sceneprofileManagement = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Stock Pilot");
+        stage.setScene(sceneprofileManagement);
+        stage.show();
+
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
