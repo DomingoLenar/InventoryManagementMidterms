3b1e16cb38788bff9154c853b61197a81fc637fb
Lenar R. Domingo <2233894@slu.edu.ph>
2024-03-24 22:55:01 +0800

    + Module Info Exports and Opens

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -1,15 +1,25 @@
-
 module com.example.inventorymanagement {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
 
+    // Controllers
     exports com.example.inventorymanagement.client.common.controllers;
     opens com.example.inventorymanagement.client.common.controllers to javafx.fxml;
     exports com.example.inventorymanagement.client.admin.controllers;
     opens com.example.inventorymanagement.client.admin.controllers to javafx.fxml;
+    exports com.example.inventorymanagement.client.purchaser.controllers;
+    opens com.example.inventorymanagement.client.purchaser.controllers to javafx.fxml;
+    exports com.example.inventorymanagement.client.sales.controllers;
+    opens com.example.inventorymanagement.client.sales.controllers to javafx.fxml;
+
+    // Views
     exports com.example.inventorymanagement.client.common.views;
     opens com.example.inventorymanagement.client.common.views to javafx.fxml;
     exports com.example.inventorymanagement.client.admin.views;
     opens com.example.inventorymanagement.client.admin.views to javafx.fxml;
+    exports com.example.inventorymanagement.client.purchaser.views;
+    opens com.example.inventorymanagement.client.purchaser.views to javafx.fxml;
+    exports com.example.inventorymanagement.client.sales.views;
+    opens com.example.inventorymanagement.client.sales.views to javafx.fxml;
 }
