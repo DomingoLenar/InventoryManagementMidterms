72b08f0443c1fc0e99f1baf75ff4ec7b62fd11f8
Lenar R. Domingo <2233894@slu.edu.ph>
2024-03-24 18:45:17 +0800

    + Module and Pom Changes

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -1,14 +1,11 @@
-
 module com.example.inventorymanagement {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
 
 
-    exports com.example.inventorymanagement.client.controller;
-    opens com.example.inventorymanagement.client.controller to javafx.fxml;
-    exports com.example.inventorymanagement.client.controller.welcome;
-    opens com.example.inventorymanagement.client.controller.welcome to javafx.fxml;
-    exports com.example.inventorymanagement.client.controller.login;
-    opens com.example.inventorymanagement.client.controller.login to javafx.fxml;
+    exports com.example.inventorymanagement.client.common.controller;
+    opens com.example.inventorymanagement.client.common.controller to javafx.fxml;
+    exports com.example.inventorymanagement.client.common.views;
+    opens com.example.inventorymanagement.client.common.views to javafx.fxml;
 }
