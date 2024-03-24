0408df1232128c5a479dca8eb350901da50455d6
Lenar R. Domingo <2233894@slu.edu.ph>
2024-03-24 22:30:15 +0800

    + Module Info Exports and Opens

--- InventoryManagement/src/main/java/module-info.java
+++ InventoryManagement/src/main/java/module-info.java
@@ -6,6 +6,10 @@ module com.example.inventorymanagement {
 
     exports com.example.inventorymanagement.client.common.controllers;
     opens com.example.inventorymanagement.client.common.controllers to javafx.fxml;
+    exports com.example.inventorymanagement.client.admin.controllers;
+    opens com.example.inventorymanagement.client.admin.controllers to javafx.fxml;
     exports com.example.inventorymanagement.client.common.views;
     opens com.example.inventorymanagement.client.common.views to javafx.fxml;
+    exports com.example.inventorymanagement.client.admin.views;
+    opens com.example.inventorymanagement.client.admin.views to javafx.fxml;
 }
