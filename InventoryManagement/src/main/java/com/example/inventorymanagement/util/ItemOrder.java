77a29a08bda12b9d84e7fea78dd53479dd5d987c
Lestat <2233915@slu.edu.ph>
2024-03-25 12:42:51 +0800

    +itemorder utility class

--- InventoryManagement/src/main/java/com/example/inventorymanagement/util/ItemOrder.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/util/ItemOrder.java
@@ -1,4 +1,75 @@
 package com.example.inventorymanagement.util;
 
-public class ItemOrder {
+import java.io.Serializable;
+import java.util.LinkedList;
+
+public class ItemOrder implements Serializable {
+    private int orderId;
+    private User byUser;
+    private String date;
+    private LinkedList<OrderDetail> orderDetails = new LinkedList<>();
+
+    /**
+     * Default constructor
+     */
+    public ItemOrder(){
+        orderId = -1;
+        byUser = null;
+        date = null;
+    }
+
+    /**
+     * Constructor method for the class
+     * @param orderId       ID of order, would be set by the server side
+     * @param byUser        Object of User that created this item order
+     * @param date          Date when the order was created
+     * @param orderDetails  LinkedList of object OrderDetails, to store details regarding this order
+     */
+    public ItemOrder(int orderId, User byUser, String date, LinkedList<OrderDetail> orderDetails) {
+        this.orderId = orderId;
+        this.byUser = byUser;
+        this.date = date;
+        this.orderDetails = orderDetails;
+    }
+
+    /**
+     * Method to add orderDetail to orderDetails
+     * @param orderDetail object of OrderDetail
+     */
+    public void addOrderDetail(OrderDetail orderDetail){
+        orderDetails.addLast(orderDetail);
+    }
+
+    // Getter and setter below
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getByUser() {
+        return byUser;
+    }
+
+    public void setByUser(User byUser) {
+        this.byUser = byUser;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public LinkedList<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(LinkedList<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }
