16b6a34ef1629ddcd7b9ec5e4efbe15e4abea801
Georcelle <2234664@slu.edu.ph>
2024-03-26 01:02:26 +0800

    +panel for the FinancesAdmin

---
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/FinancesAdminPanel.java
@@ -0,0 +1,56 @@
+package com.example.inventorymanagement.client.admin.views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FinancesAdminPanel extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Load font
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"), 20);
+
+        // Load FXML files
+        FXMLLoader navigationBarLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarAdmin-view.fxml"));
+        FXMLLoader financesLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/finances/financesAdmin-view.fxml"));
+
+        // Load UI elements
+        BorderPane navigationBar = navigationBarLoader.load();
+        BorderPane financesPanel = financesLoader.load();
+
+        // Set up root pane
+        BorderPane root = new BorderPane();
+        root.setLeft(navigationBar);
+        root.setRight(financesPanel);
+
+        // Create scene
+        Scene sceneStockControlAdminPanel = new Scene(root, 1080, 650);
+
+        // Load icon
+        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
+        if (inputStream != null) {
+            Image image = new Image(inputStream);
+            stage.getIcons().add(image);
+        } else {
+            System.err.println("Failed to load image: logo.png");
+        }
+
+        // Set stage properties
+        stage.setTitle("Stock Pilot");
+        stage.setScene(sceneStockControlAdminPanel);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
