6b14c1fbde5e948aaf7497be0f726cfe45d00c48
Lestat <2233915@slu.edu.ph>
2024-03-26 15:08:06 +0800

    +incomplete Server class, to be populated with more logic

--- InventoryManagement/src/main/java/com/example/inventorymanagement/server/model/Server.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/server/model/Server.java
@@ -1,4 +1,14 @@
 package com.example.inventorymanagement.server.model;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Server {
+
+    public void run() throws RemoteException {
+        Registry reg = LocateRegistry.createRegistry(2018);
+        reg.rebind("user", new UserRequestInterfaceImplementation());
+
+    }
 }
