bc464eb4f94310b1655d403435347f615bd993b2
Lenar R. Domingo <2233894@slu.edu.ph>
2024-03-29 00:32:48 +0800

    + Panel Modifications on Connections to Navigation Bar

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/StockControlAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/StockControlAdminPanel.java
@@ -3,45 +3,40 @@ package com.example.inventorymanagement.client.admin.views;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import com.example.inventorymanagement.client.admin.controllers.NavigationBarAdminController;
+
 import java.io.IOException;
-import java.io.InputStream;
 
 public class StockControlAdminPanel extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"), 20);
 
-        FXMLLoader navigationBarLoader = new FXMLLoader(StockControlAdminPanel.class.getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarAdmin-view.fxml"));
-        FXMLLoader stockControlLoader = new FXMLLoader(StockControlAdminPanel.class.getResource("/com/example/inventorymanagement/client/view/stockControl/stockControlAdmin-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarAdmin-view.fxml"));
+        BorderPane navigationBar = loader.load();
 
-        BorderPane navigationBar = navigationBarLoader.load();
-        BorderPane stockControlPanel = stockControlLoader.load();
+        // Get the controller
+        NavigationBarAdminController navBarController = loader.getController();
+
+        // Create the stock control panel
+        BorderPane stockControlPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/stockControlAdmin-view.fxml"));
 
         BorderPane root = new BorderPane();
         root.setLeft(navigationBar);
         root.setRight(stockControlPanel);
 
-        Scene sceneStockControlAdminPanel = new Scene(root, 1080, 650);
-
-        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
-        if (inputStream != null) {
-            Image image = new Image(inputStream);
-            stage.getIcons().add(image);
-        } else {
-            System.err.println("Failed to load image: logo.png");
-        }
-
+        Scene scene = new Scene(root, 1080, 650);
+        stage.setScene(scene);
         stage.setTitle("Stock Pilot");
-        stage.setScene(sceneStockControlAdminPanel);
         stage.show();
 
-        stage.setResizable(false);
+        // Set the main BorderPane reference in the navigation bar controller
+        navBarController.setMainBorderPane(root);
     }
 
     public static void main(String[] args) {
