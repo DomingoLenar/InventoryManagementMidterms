f597a70990d81221c2cb3ade011c53d9d69ccceb
Lou <2233672@slu.edu.ph>
2024-03-31 00:56:23 +0800

    Add onLogin() in LoginController.java responsible for handling an event which logs a user in the server

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/LoginController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/LoginController.java
@@ -1,9 +1,17 @@
 package com.example.inventorymanagement.client.common.controllers;
 
+import com.example.inventorymanagement.client.model.ClientCallbackImpl;
+import com.example.inventorymanagement.util.exceptions.AlreadyLoggedInException;
+import com.example.inventorymanagement.util.exceptions.UserExistenceException;
+import com.example.inventorymanagement.util.objects.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -28,5 +36,17 @@ public class LoginController {
     private void addHoverEffect(Button button) {
         button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#FFFFFF, -10%);"));
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #FFFFFF;"));
+    }
+    @FXML
+    public void OnLogin(ActionEvent actionEvent) throws IOException, UserExistenceException, AlreadyLoggedInException { // TODO: validate by server rmi
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        User user = new User(username, password, null);
+        mainController.setClientCallback(new ClientCallbackImpl(user));
+        mainController.getUserService().login(mainController.getClientCallback());
+
+        Node source = (Node) actionEvent.getSource();
+        source.getScene().getWindow().hide();
+        mainController.displayAdminMainMenu();
     }
 }
