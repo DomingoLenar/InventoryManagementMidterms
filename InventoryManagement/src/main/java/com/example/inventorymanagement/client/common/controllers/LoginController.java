fddc565cf982dcc227a6a22da0aca43fd9f32220
Lou <2233672@slu.edu.ph>
2024-03-31 11:48:10 +0800

    Implement the communication of client-server for logging session of users in LoginController class

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/LoginController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/LoginController.java
@@ -7,15 +7,19 @@ import com.example.inventorymanagement.util.objects.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
 
 public class LoginController {
-    @FXML
-    private BorderPane borderPaneLogin;
     @FXML
     private Button loginButtonLogin;
     @FXML
@@ -38,15 +42,38 @@ public class LoginController {
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #FFFFFF;"));
     }
     @FXML
-    public void OnLogin(ActionEvent actionEvent) throws IOException, UserExistenceException, AlreadyLoggedInException { // TODO: validate by server rmi
+    public void OnLogin(ActionEvent actionEvent) {
         String username = usernameField.getText();
         String password = passwordField.getText();
         User user = new User(username, password, null);
-        mainController.setClientCallback(new ClientCallbackImpl(user));
-        mainController.getUserService().login(mainController.getClientCallback());
+        try {
+            mainController.setClientCallback(new ClientCallbackImpl(user));
+            User currentUser = mainController.getUserService().login(mainController.getClientCallback());
+            String role = currentUser.role;
 
-        Node source = (Node) actionEvent.getSource();
-        source.getScene().getWindow().hide();
-        mainController.displayAdminMainMenu();
+            Node source = (Node) actionEvent.getSource();
+            source.getScene().getWindow().hide();
+
+            switch (role) {
+                case "admin":
+                    mainController.displayAdminMainMenu();
+                    break;
+                case "sales":
+                    mainController.displaySalesMainMenu();
+                    break;
+                case "purchaser":
+                    mainController.displayPurchaserMainMenu();
+                    break;
+            }
+        } catch (RemoteException remoteException) {
+            throw new RuntimeException("RMI Server error");
+        } catch (UserExistenceException userExistenceException) {
+
+        } catch (AlreadyLoggedInException alreadyLoggedInException) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // todo: handle the exception using javafx components
     }
 }
