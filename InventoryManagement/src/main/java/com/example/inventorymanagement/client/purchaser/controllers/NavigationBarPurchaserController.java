c7ebeec9de1850e02c2fb6efd0eb10c8132fde02
Lenar <2233894@slu.edu.ph>
2024-04-04 00:01:54 +0800

    + Connection of Navigation Bar to Stock Control (Purchaser)

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/controllers/NavigationBarPurchaserController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/controllers/NavigationBarPurchaserController.java
@@ -1,8 +1,11 @@
 package com.example.inventorymanagement.client.purchaser.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class NavigationBarPurchaserController {
     @FXML
@@ -11,6 +14,14 @@ public class NavigationBarPurchaserController {
     private Button stockControlButtonPurchaser;
     @FXML
     private Button profileButtonPurchaser;
+
+    // Reference to the main BorderPane
+    private BorderPane mainBorderPane;
+
+    // Setter for main BorderPane
+    public void setMainBorderPane(BorderPane mainBorderPane) {
+        this.mainBorderPane = mainBorderPane;
+    }
 
     @FXML
     public BorderPane getBorderPaneNavigationBarPurchaser() { return borderPaneNavigationBarPurchaser;}
@@ -23,6 +34,9 @@ public class NavigationBarPurchaserController {
 
     @FXML
     private void initialize() {
+        // Handle button clicks
+        stockControlButtonPurchaser.setOnAction(event -> loadStockControlPanel());
+
         addHoverEffect(stockControlButtonPurchaser);
         addHoverEffect(profileButtonPurchaser);
     }
@@ -30,5 +44,15 @@ public class NavigationBarPurchaserController {
     private void addHoverEffect(Button button) {
         button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: #967373;"));
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+    }
+
+    private void loadStockControlPanel() {
+        // Load Stock Control panel
+        try {
+            BorderPane stockControlPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/stockControlPurchaser-view.fxml"));
+            mainBorderPane.setRight(stockControlPanel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/views/StockControlPurchaserPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/views/StockControlPurchaserPanel.java
@@ -3,45 +3,40 @@ package com.example.inventorymanagement.client.purchaser.views;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import com.example.inventorymanagement.client.purchaser.controllers.NavigationBarPurchaserController;
+
 import java.io.IOException;
-import java.io.InputStream;
 
 public class StockControlPurchaserPanel extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"), 20);
 
-        FXMLLoader navigationBarLoader = new FXMLLoader(StockControlPurchaserPanel.class.getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarPurchaser-view.fxml"));
-        FXMLLoader stockControlLoader = new FXMLLoader(StockControlPurchaserPanel.class.getResource("/com/example/inventorymanagement/client/view/stockControl/stockControlPurchaser-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarPurchaser-view.fxml"));
+        BorderPane navigationBar = loader.load();
 
-        BorderPane navigationBar = navigationBarLoader.load();
-        BorderPane stockControlPanel = stockControlLoader.load();
+        // Get the controller
+        NavigationBarPurchaserController navBarController = loader.getController();
+
+        // Create the stock control panel
+        BorderPane stockControlPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/stockControlPurchaser-view.fxml"));
 
         BorderPane root = new BorderPane();
         root.setLeft(navigationBar);
         root.setRight(stockControlPanel);
 
-        Scene sceneStockControlPurchaserPanel = new Scene(root, 1080, 650);
-
-        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
-        if (inputStream != null) {
-            Image image = new Image(inputStream);
-            stage.getIcons().add(image);
-        } else {
-            System.err.println("Failed to load image: logo.png");
-        }
-
+        Scene scene = new Scene(root, 1080, 650);
+        stage.setScene(scene);
         stage.setTitle("Stock Pilot");
-        stage.setScene(sceneStockControlPurchaserPanel);
         stage.show();
 
-        stage.setResizable(false);
+        // Set the main BorderPane reference in the navigation bar controller
+        navBarController.setMainBorderPane(root);
     }
 
     public static void main(String[] args) {
