37015d1b9d6c0e5048841cd1393444462d11908b
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2024-04-04 15:17:54 +0800

    + Optimized AddItem handling

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/StockControlAdminModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/StockControlAdminModel.java
@@ -4,23 +4,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class StockControlAdminModel {
     @FXML
     public void handleAddItem() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/addItem-view.fxml"));
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/addItem-view.fxml"));
+                Parent root = fxmlLoader.load();
 
-            Parent root = fxmlLoader.load();
+                Dialog<ButtonType> dialog = new Dialog<>();
+                dialog.setTitle("Add Item");
 
-            Stage stage = new Stage();
-            stage.setTitle("Add Item");
-            stage.setScene(new Scene(root));
+                DialogPane dialogPane = new DialogPane();
+                dialogPane.setContent(root);
+                dialog.setDialogPane(dialogPane);
 
-            stage.show();
+                dialog.initStyle(StageStyle.UNDECORATED);
+
+                ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+                dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
+
+                Button okButton = (Button) dialog.getDialogPane().lookupButton(okButtonType);
+                okButton.setOnAction(event -> {
+                    //TODO: Logic from AddItemModel for adding items using GSONProcessing
+                });
+
+                Button cancelButton = new Button("Cancel");
+                cancelButton.setOnAction(event -> dialog.close());
+
+                dialog.getDialogPane().getChildren().addAll(okButton, cancelButton);
+
+                dialog.showAndWait();
         } catch (IOException e) {
             e.printStackTrace();
         }
