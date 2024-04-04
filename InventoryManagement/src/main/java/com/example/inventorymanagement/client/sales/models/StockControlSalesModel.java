ce5912530f567774a2fb7ab66c7722201a462ff0
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2024-04-04 15:33:28 +0800

    + StockControlSalesModel + StockControlSalesModel.handleSalesInvoice

---
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/models/StockControlSalesModel.java
@@ -0,0 +1,49 @@
+package com.example.inventorymanagement.client.sales.models;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class StockControlSalesModel {
+
+    /**
+     * Handle Action Event when clicking createSalesInvoiceButton
+     */
+    @FXML
+    public void handleSalesInvoice() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/createSalesInvoice-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.setTitle("Sales Invoice");
+
+            DialogPane dialogPane = new DialogPane();
+            dialogPane.setContent(root);
+            dialog.setDialogPane(dialogPane);
+
+            dialog.initStyle(StageStyle.UNDECORATED);
+
+            ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
+
+            Button okButton = (Button) dialog.getDialogPane().lookupButton(okButtonType);
+            okButton.setOnAction(event -> {
+                //TODO: Logic from CreateSalesInvoiceModel for invoicing items using GSONProcessing
+            });
+
+            Button cancelButton = new Button("Cancel");
+            cancelButton.setOnAction(event -> dialog.close());
+
+            dialog.getDialogPane().getChildren().addAll(okButton, cancelButton);
+
+            dialog.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
