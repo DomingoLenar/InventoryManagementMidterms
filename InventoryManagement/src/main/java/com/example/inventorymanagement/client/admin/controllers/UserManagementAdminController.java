d6e7fa9b0716b1951a3f0ff625cda7edbf19d991
Georcelle <2234664@slu.edu.ph>
2024-04-06 00:30:11 +0800

    + added hover effect on 'addUser' button

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/UserManagementAdminController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/UserManagementAdminController.java
@@ -1,24 +1,26 @@
 package com.example.inventorymanagement.client.admin.controllers;
 
 import com.example.inventorymanagement.client.common.controllers.ControllerInterface;
+import com.example.inventorymanagement.util.objects.User;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
 
+import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.ResourceBundle;
 
-public class UserManagementAdminController implements ControllerInterface {
+public class UserManagementAdminController implements Initializable, ControllerInterface {
 
     @FXML
     private BorderPane borderPaneUserManagement;
     @FXML
     private TextField searchTextField;
     @FXML
-    private TableView userManagementTableView;
+    private TableView<User> userManagementTableView;
     @FXML
     private Button addUserButton;
 
@@ -33,9 +35,11 @@ public class UserManagementAdminController implements ControllerInterface {
     public Button getAddUserButton() {
         return addUserButton;
     }
-    public TableView getUserManagementTableView(){
+
+    public TableView<User> getUserManagementTableView() {
         return userManagementTableView;
     }
+
     @Override
     public void fetchAndUpdate() throws RemoteException {
     }
@@ -43,8 +47,17 @@ public class UserManagementAdminController implements ControllerInterface {
     public String getCurrentPanel() throws RemoteException {
         return "UserManagementAdmin"; // Return the name of this panel
     }
+    @FXML
+    private void initialize() {
+        addHoverEffect(addUserButton);
+    }
+
     private void addHoverEffect(Button button) {
         button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
