c6f2f0fef44e6446f346d8e5cdc535aef56e81a8
Georcelle <2234664@slu.edu.ph>
2024-04-06 03:27:58 +0800

    + fixed the FXML loader and Dashboard Loader

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/DashboardAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/DashboardAdminPanel.java
@@ -1,57 +1,41 @@
 package com.example.inventorymanagement.client.admin.views;
 
-import com.example.inventorymanagement.client.admin.controllers.DashboardAdminController;
+import com.example.inventorymanagement.client.admin.controllers.NavigationBarAdminController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class DashboardAdminPanel extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        // Load font
+    public void start(Stage stage) throws IOException {
+
         Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"), 20);
 
-        // Load FXML files
-        FXMLLoader navigationBarLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarAdmin-view.fxml"));
-        FXMLLoader dashboardLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/dashboard/dashboardAdmin-view.fxml"));
-        dashboardLoader.setController(new DashboardAdminController());
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/navigationBar/navigationBarAdmin-view.fxml"));
+        BorderPane navigationBar = loader.load();
 
-        // Load UI elements
-        BorderPane navigationBar = navigationBarLoader.load();
-        BorderPane dashboardPanel = dashboardLoader.load();
+        // Get the controller
+        NavigationBarAdminController navBarAdminController = loader.getController();
 
-        // Get the controller instance
-        DashboardAdminController controller = dashboardLoader.getController();
+        // Create the dashboard panel
+        BorderPane dashboardAdminPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/dashboard/dashboardAdmin-view.fxml"));
 
-        // Set up root pane
         BorderPane root = new BorderPane();
         root.setLeft(navigationBar);
-        root.setRight(dashboardPanel);
+        root.setRight(dashboardAdminPanel);
 
-        // Create scene
-        Scene sceneStockControlAdminPanel = new Scene(root, 1080, 650);
-
-        // Load icon
-        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
-        if (inputStream != null) {
-            Image image = new Image(inputStream);
-            stage.getIcons().add(image);
-        } else {
-            System.err.println("Failed to load image: logo.png");
-        }
-
-        // Set stage properties
+        Scene scene = new Scene(root, 1080, 650);
+        stage.setScene(scene);
         stage.setTitle("Stock Pilot");
-        stage.setScene(sceneStockControlAdminPanel);
-        stage.setResizable(false);
         stage.show();
+
+        // Set the main BorderPane reference in the navigation bar controller
+        navBarAdminController.setMainBorderPane(root);
     }
 
     public static void main(String[] args) {
