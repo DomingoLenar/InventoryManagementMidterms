361120e7d2166aa6007cc3dcfdb9a059def1c7b1
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2024-04-06 02:56:41 +0800

    + Refactoring

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/AddItemAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/AddItemAdminPanel.java
@@ -3,29 +3,34 @@ package com.example.inventorymanagement.client.admin.views;
 import com.example.inventorymanagement.client.admin.controllers.NavigationBarAdminController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class AddItemAdminPanel extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"), 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/addItem-view.fxml"));
-        BorderPane navigationBar = loader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(AddItemAdminPanel.class.getResource("/com/example/inventorymanagement/client/view/stockControl/addItem-view.fxml"));
+        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
 
-        // Get the controller
-        NavigationBarAdminController navBarController = loader.getController();
+        if (inputStream != null) {
+            Image image = new Image(inputStream);
+            stage.getIcons().add(image);
+        } else {
+            System.err.println("Failed to load image: logo.png");
+        }
 
-        // Create the stock control panel
-        BorderPane stockControlPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/createSalesInvoice-view.fxml"));
+        Scene sceneLogin = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Stock Pilot");
+        stage.setScene(sceneLogin);
+        stage.show();
 
-        BorderPane root = new BorderPane();
-        root.setLeft(navigationBar);
-        root.setRight(stockControlPanel);
-
-        navBarController.setMainBorderPane(root);
+        stage.setResizable(false);
     }
 }
