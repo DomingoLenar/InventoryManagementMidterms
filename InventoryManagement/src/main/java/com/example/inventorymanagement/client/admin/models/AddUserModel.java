478c99a2805ba1c88f2b2ee6ed42e84c6f8a150d
MarvenLuis <2234244@slu.edu.ph>
2024-04-05 19:08:13 +0800

    + Edited the registry for the model

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/AddUserModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/AddUserModel.java
@@ -19,7 +19,7 @@ public class AddUserModel {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
 
-            UserRequestInterface userRequest = (UserRequestInterface) registry.lookup("user");
+            UserRequestInterface userRequest = (UserRequestInterface) registry.lookup("userRequest");
 
             ClientCallback cB = new ClientCallbackImpl(requestBy);
 
