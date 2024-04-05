e192ef350dd9d24ce0080956da05c2756fd58cc2
Georcelle <2234664@slu.edu.ph>
2024-04-06 03:55:11 +0800

    + minor adjustment

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/NavigationBarSalesController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/NavigationBarSalesController.java
@@ -78,7 +78,7 @@ public class NavigationBarSalesController implements ControllerInterface {
 
     private void loadProfileManagementPanel() {
         try {
-            BorderPane profileManagementPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/profileManagement/profileManagementChangePass-view.fxml"));
+            BorderPane profileManagementPanel = FXMLLoader.load(getClass().getResource("/com/example/inventorymanagement/client/view/profileManagement/profileManagement-view.fxml"));
             mainBorderPane.setRight(profileManagementPanel);
         } catch (IOException e) {
             e.printStackTrace();
