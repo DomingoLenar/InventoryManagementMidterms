faf307022adbdbd304b9051b4daa997f5cde51ea
MarvenLuis <2234244@slu.edu.ph>
2024-04-06 18:39:31 +0800

    + Modified the parameters for the RemoveItemListingService

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/microservices/RemoveItemListingService.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/microservices/RemoveItemListingService.java
@@ -14,12 +14,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RemoveItemListingService {
-    public boolean process (User requestBy, Item item){
+    public boolean process (Registry registry, ClientCallback cB , Item item){
         try {
-            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
-            ItemRequestInterface ItemRequest = (ItemRequestInterface) registry.lookup("item");
 
-            ClientCallback cB = new ClientCallbackImpl(requestBy);
+            ItemRequestInterface ItemRequest = (ItemRequestInterface) registry.lookup("item");
 
             return ItemRequest.removeItemListing(cB,item);
 
