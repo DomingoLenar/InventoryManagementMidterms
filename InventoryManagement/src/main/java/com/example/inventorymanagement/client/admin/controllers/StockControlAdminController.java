fbf42bf8b6d922d2ec4b4eded715b3a1d6d2247a
Lou <2233672@slu.edu.ph>
2024-04-07 01:09:29 +0800

    some updates

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/StockControlAdminController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/StockControlAdminController.java
@@ -1,10 +1,14 @@
 package com.example.inventorymanagement.client.admin.controllers;
 
+import com.example.inventorymanagement.client.common.controllers.MainController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class StockControlAdminController {
     @FXML
@@ -21,6 +25,11 @@ public class StockControlAdminController {
     private TextField searchFieldAdmin;
     @FXML
     private TableView stockControlAdminTable;
+    private MainController mainController;
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 
     @FXML
     public BorderPane getBorderPaneStockControlAdmin() { return borderPaneStockControlAdmin;}
@@ -54,5 +63,40 @@ public class StockControlAdminController {
     private void addHoverEffect(Button button) {
         button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+    }
+
+    @FXML
+    public void handleAddItem() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/inventorymanagement/client/view/stockControl/addItem-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.setTitle("Add Item");
+
+            DialogPane dialogPane = new DialogPane();
+            dialogPane.setContent(root);
+            dialog.setDialogPane(dialogPane);
+
+            dialog.initStyle(StageStyle.UNDECORATED);
+
+            ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
+
+            Button okButton = (Button) dialog.getDialogPane().lookupButton(okButtonType);
+            okButton.setOnAction(event -> {
+                //TODO: Logic from AddItemModel for adding items using GSONProcessing
+            });
+
+            Button cancelButton = new Button("Cancel");
+            cancelButton.setOnAction(event -> dialog.close());
+
+            dialog.getDialogPane().getChildren().addAll(okButton, cancelButton);
+
+            dialog.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
