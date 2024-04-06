b875039ebd02267ee9727d3d89de37f4e73ba4ff
Lou <2233672@slu.edu.ph>
2024-04-07 01:34:34 +0800

    some updates

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/StockControlAdminController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/StockControlAdminController.java
@@ -1,6 +1,12 @@
 package com.example.inventorymanagement.client.admin.controllers;
 
+import com.example.inventorymanagement.client.admin.models.StockControlAdminModel;
 import com.example.inventorymanagement.client.common.controllers.MainController;
+import com.example.inventorymanagement.util.ClientCallback;
+import com.example.inventorymanagement.util.ControllerInterface;
+import com.example.inventorymanagement.util.requests.ItemOrderRequestInterface;
+import com.example.inventorymanagement.util.requests.ItemRequestInterface;
+import com.example.inventorymanagement.util.requests.UserRequestInterface;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,8 +15,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 
-public class StockControlAdminController {
+public class StockControlAdminController implements ControllerInterface {
     @FXML
     private BorderPane borderPaneStockControlAdmin;
     @FXML
@@ -26,9 +34,16 @@ public class StockControlAdminController {
     @FXML
     private TableView stockControlAdminTable;
     private MainController mainController;
+    public StockControlAdminController() {
+
+    }
+    public StockControlAdminController(ClientCallback clientCallback, UserRequestInterface userService, ItemOrderRequestInterface iOService, ItemRequestInterface itemService, Registry registry) {
+        StockControlAdminModel stockControlAdminModel = new StockControlAdminModel(registry, clientCallback); // use this on events of stockcontroladminview
+    }
+
 
     public void setMainController(MainController mainController) {
-        this.mainController = mainController;
+        this.mainController = mainController; // or this main...getSERVICES....actions()
     }
 
     @FXML
@@ -98,5 +113,15 @@ public class StockControlAdminController {
             e.printStackTrace();
 
         }
+    }
+
+    @Override
+    public void fetchAndUpdate() throws RemoteException {
+        
+    }
+
+    @Override
+    public String getObjectsUsed() throws RemoteException {
+        return null;
     }
 }
