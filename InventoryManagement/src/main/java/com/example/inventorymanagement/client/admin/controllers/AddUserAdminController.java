4841b68db1ca47b551a26d1753b97f2139f7e77f
Georcelle <2234664@slu.edu.ph>
2024-04-07 03:16:12 +0800

    + fixed the structure and registry bug

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/AddUserAdminController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/controllers/AddUserAdminController.java
@@ -1,4 +1,5 @@
 package com.example.inventorymanagement.client.admin.controllers;
+
 import com.example.inventorymanagement.client.admin.models.AddUserAdminModel;
 import com.example.inventorymanagement.client.admin.views.AddUserAdminPanel;
 import com.example.inventorymanagement.util.ClientCallback;
@@ -16,11 +17,10 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
-import java.util.LinkedList;
 import java.util.ResourceBundle;
 
-
 public class AddUserAdminController extends Application implements Initializable, ControllerInterface {
+
     @FXML
     private ComboBox<String> roleComboBox;
 
@@ -29,17 +29,36 @@ public class AddUserAdminController extends Application implements Initializable
 
     @FXML
     private TextField passwordField;
+
     @FXML
     private Button saveButton;
 
-    // Reference to the AddUserAdminModel and AddUserAdminPanel
-    AddUserAdminModel addUserAdminModel = new AddUserAdminModel();
-    AddUserAdminPanel addUserAdminPanel = new AddUserAdminPanel();
-    // Declare clientCallback
+    private AddUserAdminModel addUserAdminModel;
+    private AddUserAdminPanel addUserAdminPanel;
     private ClientCallback clientCallback;
     private Registry registry;
 
-    // Initialize clientCallback before using it
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        roleComboBox.getItems().addAll("Sales Person", "Purchaser");
+        roleComboBox.setPromptText("Set Role As...");
+        Font font = new Font("Share Tech Mono", 15);
+        roleComboBox.setStyle("-fx-font-family: '" + font.getFamily() + "'; -fx-font-size: " + font.getSize() + "px;");
+        addHoverEffect(saveButton);
+        addUserAdminModel = new AddUserAdminModel(registry, clientCallback);
+        addUserAdminPanel = new AddUserAdminPanel();
+    }
+
+    private void addHoverEffect(Button button) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        addUserAdminPanel.start(stage);
+    }
+
     public void setClientCallback(ClientCallback clientCallback) {
         this.clientCallback = clientCallback;
     }
@@ -61,29 +80,10 @@ public class AddUserAdminController extends Application implements Initializable
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        roleComboBox.getItems().addAll("Sales Person", "Purchaser");
-        roleComboBox.setPromptText("Set Role As...");
-        Font font = new Font("Share Tech Mono", 15);
-        roleComboBox.setStyle("-fx-font-family: '" + font.getFamily() + "'; -fx-font-size: " + font.getSize() + "px;");
-        addHoverEffect(saveButton);
-    }
-
-    private void addHoverEffect(Button button) {
-        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
-        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        addUserAdminPanel.start(stage);
-    }
-
-    // Method that will fetch data from model and insert icontroller runt into ui elements
     public void fetchAndUpdate() throws RemoteException {
     }
 
-    // Method for getting what specific panel is currently on display
+    @Override
     public String getObjectsUsed() throws RemoteException {
         return "User";
     }
@@ -91,27 +91,18 @@ public class AddUserAdminController extends Application implements Initializable
     @FXML
     private void handleSaveButtonClick() {
         try {
-            // Get the data from the UI elements
             String username = usernameField.getText();
             String password = passwordField.getText();
             String role = roleComboBox.getValue();
-
-            // Create a new User object
             User newUser = new User(username, password, role);
-
-            // Perform user addition operation using the addUserAdminModel instance
             boolean success = addUserAdminModel.addUser(newUser);
-
             if (success) {
                 throw new RuntimeException("User added successfully.");
             } else {
                 throw new RuntimeException("Failed to add user. Make sure to enter a 7 character password or check if the username has duplicates");
             }
         } catch (Exception e) {
-            // Handle the exception here (display error message, log, etc.)
             e.printStackTrace();
         }
     }
 }
-
-
