ddb6b6a0f527ae9f770cab392c60c126b7b41824
Lenar <2233894@slu.edu.ph>
2024-04-06 23:25:16 +0800

    + CreateSalesInvoiceAdminModel

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/CreateSalesInvoiceAdminModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/CreateSalesInvoiceAdminModel.java
@@ -1,5 +1,26 @@
 package com.example.inventorymanagement.client.admin.models;
 
+import com.example.inventorymanagement.client.microservices.CreateSalesInvoiceService;
+import com.example.inventorymanagement.util.ClientCallback;
+import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
+import com.example.inventorymanagement.util.exceptions.OutOfRoleException;
+import com.example.inventorymanagement.util.objects.ItemOrder;
+
+import java.rmi.registry.Registry;
+
 public class CreateSalesInvoiceAdminModel {
 
+    private Registry registry;
+    private ClientCallback clientCallback;
+    private ItemOrder salesInvoice;
+
+    public CreateSalesInvoiceAdminModel(Registry registry, ClientCallback clientCallback, ItemOrder salesInvoice) {
+        this.registry = registry;
+        this.clientCallback = clientCallback;
+        this.salesInvoice = salesInvoice;
+    }
+
+    public boolean createSalesInvoice() throws OutOfRoleException, NotLoggedInException {
+        return CreateSalesInvoiceService.process(registry, clientCallback, salesInvoice);
+    }
 }
