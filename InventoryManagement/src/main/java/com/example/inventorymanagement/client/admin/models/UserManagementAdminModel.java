d2406bf813584aaa8555ae48380f9abdc973fffa
Georcelle <2234664@slu.edu.ph>
2024-04-08 02:06:22 +0800

    + made the model class for User Management Admin

---
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/UserManagementAdminModel.java
@@ -0,0 +1,29 @@
+package com.example.inventorymanagement.client.admin.models;
+
+import com.example.inventorymanagement.client.microservices.FetchListOfUsersService;
+import com.example.inventorymanagement.util.ClientCallback;
+import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
+import com.example.inventorymanagement.util.exceptions.OutOfRoleException;
+import com.example.inventorymanagement.util.objects.User;
+import java.rmi.registry.Registry;
+import java.util.LinkedList;
+
+public class UserManagementAdminModel {
+    private Registry registry;
+    private ClientCallback clientCallback;
+
+    public UserManagementAdminModel(Registry registry, ClientCallback clientCallback) {
+        this.registry = registry;
+        this.clientCallback = clientCallback;
+    }
+
+    public LinkedList<User> fetchListOfUsers() {
+        try {
+            return FetchListOfUsersService.process(clientCallback, registry);
+        } catch (OutOfRoleException | NotLoggedInException e) {
+            // Handle exceptions appropriately
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
