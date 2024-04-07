7803e75e3ef4c083e4b4347d02651f3e1a4970a8
Georcelle <2234664@slu.edu.ph>
2024-04-08 02:02:17 +0800

    + made the controller class runnable

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/ProfileManagementChangePassController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/common/controllers/ProfileManagementChangePassController.java
@@ -1,18 +1,30 @@
 package com.example.inventorymanagement.client.common.controllers;
 
+import com.example.inventorymanagement.client.admin.models.ProfileManagementAdminModel;
+import com.example.inventorymanagement.client.admin.views.ProfileManagementAdminPanel;
+import com.example.inventorymanagement.client.common.models.ProfileManagementChangePassModel;
+import com.example.inventorymanagement.client.common.views.ProfileManagementChangePassPanel;
+import com.example.inventorymanagement.util.ClientCallback;
+import com.example.inventorymanagement.util.ControllerInterface;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.net.URL;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.ResourceBundle;
 
 // todo: handle action event and submit it to rmi server
-public class ProfileManagementChangePassController {
+public class ProfileManagementChangePassController extends Application implements Initializable, ControllerInterface {
     @FXML
-    private BorderPane borderPaneProfileManagement;
+    private BorderPane borderPaneProfileManagementChangePass;
     @FXML
     private ImageView personIconImage;
     @FXML
@@ -27,9 +39,19 @@ public class ProfileManagementChangePassController {
     private TextField newPasswordTextField;
     @FXML
     private Button saveButton;
+    private MainController mainController;
 
-    public BorderPane getBorderPaneProfileManagement() {
-        return borderPaneProfileManagement;
+    private ClientCallback clientCallback;
+    private Registry registry;
+    private ProfileManagementChangePassModel profileManagementChangePassModel;
+    private ProfileManagementChangePassPanel profileManagementChangePassPanel = new ProfileManagementChangePassPanel();
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+
+    public BorderPane getBorderPaneProfileManagementChangePass() {
+        return borderPaneProfileManagementChangePass;
     }
     public ImageView getPersonIconImage(){
         return personIconImage;
@@ -60,8 +82,21 @@ public class ProfileManagementChangePassController {
     public void fetchAndUpdate() throws RemoteException {
     }
 
-    public String getCurrentPanel() throws RemoteException {
+    public String getObjectsUsed() throws RemoteException {
         return "ProfileManagementChangePass"; // Return the name of this panel
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        profileManagementChangePassPanel = new ProfileManagementChangePassPanel();
+        profileManagementChangePassPanel.start(stage);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // initialize the model and panel objects
+        profileManagementChangePassPanel = new ProfileManagementChangePassPanel();
+        profileManagementChangePassModel = new ProfileManagementChangePassModel(registry, clientCallback);
     }
 }
 
