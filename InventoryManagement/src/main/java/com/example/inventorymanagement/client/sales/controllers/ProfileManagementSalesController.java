ed8a89dda3b4ab1f3d93427a1cccd2f75b96e902
Georcelle <2234664@slu.edu.ph>
2024-04-08 01:11:12 +0800

    + made the controller class runnable

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/ProfileManagementSalesController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/ProfileManagementSalesController.java
@@ -1,6 +1,14 @@
 package com.example.inventorymanagement.client.sales.controllers;
 
+import com.example.inventorymanagement.client.admin.models.ProfileManagementAdminModel;
+import com.example.inventorymanagement.client.common.controllers.MainController;
+import com.example.inventorymanagement.client.purchaser.models.ProfileManagementPurchaserModel;
+import com.example.inventorymanagement.client.purchaser.views.ProfileManagementPurchaserPanel;
+import com.example.inventorymanagement.client.sales.models.ProfileManagementSalesModel;
+import com.example.inventorymanagement.client.sales.views.ProfileManagementSalesPanel;
+import com.example.inventorymanagement.util.ClientCallback;
 import com.example.inventorymanagement.util.ControllerInterface;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,12 +16,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
 
-public class ProfileManagementSalesController  implements Initializable, ControllerInterface {
+public class ProfileManagementSalesController  extends Application implements Initializable, ControllerInterface {
     @FXML
     private BorderPane borderPaneProfileManagement;
     @FXML
@@ -28,6 +38,12 @@ public class ProfileManagementSalesController  implements Initializable, Control
     private Button changePasswordButton;
     @FXML
     private Button logoutButton;
+    private MainController mainController;
+
+    private ClientCallback clientCallback;
+    private Registry registry;
+    private ProfileManagementSalesModel profileManagementSalesModel;
+    private ProfileManagementSalesPanel profileManagementSalesPanel;
     public void fetchAndUpdate() throws RemoteException {
         // No implementation needed yet in this controller
     }
@@ -61,11 +77,7 @@ public class ProfileManagementSalesController  implements Initializable, Control
     public Button getChangePasswordButton(){
         return changePasswordButton;
     }
-    private void initialize() {
-        addHoverEffect(logoutButton);
-        addHoverEffect(changePasswordButton);
 
-    }
     private void addHoverEffect(Button button) {
         button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
         button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
@@ -74,5 +86,13 @@ public class ProfileManagementSalesController  implements Initializable, Control
         changeUserAccountComboBox.setPromptText("Change Role...");
         Font font = new Font("Share Tech Mono", 15);
         changeUserAccountComboBox.setStyle("-fx-font-family: '" + font.getFamily() + "'; -fx-font-size: " + font.getSize() + "px;");
+        addHoverEffect(logoutButton);
+        addHoverEffect(changePasswordButton);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        profileManagementSalesPanel = new ProfileManagementSalesPanel();
+        profileManagementSalesPanel.start(stage);
     }
 }
