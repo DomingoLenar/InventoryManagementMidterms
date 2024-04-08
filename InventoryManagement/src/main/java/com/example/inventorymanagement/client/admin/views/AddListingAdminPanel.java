25acc497a6315162d77bc7fd05561c7c0b2409a2
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2024-04-09 05:51:14 +0800

    + AddListingAdminPanel refactors

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/AddListingAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/AddListingAdminPanel.java
@@ -11,23 +11,6 @@ import java.io.InputStream;
 
 public class AddListingAdminPanel {
     public void start(Stage stage) throws IOException {
-        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
 
-        FXMLLoader fxmlLoader = new FXMLLoader(AddItemAdminPanel.class.getResource("/com/example/inventorymanagement/client/view/stockControl/addListing-view.fxml"));
-        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
-
-        if (inputStream != null) {
-            Image image = new Image(inputStream);
-            stage.getIcons().add(image);
-        } else {
-            System.err.println("Failed to load image: logo.png");
-        }
-
-        Scene sceneLogin = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Stock Pilot");
-        stage.setScene(sceneLogin);
-        stage.show();
-
-        stage.setResizable(false);
     }
 }
