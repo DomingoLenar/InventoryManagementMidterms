474a7c4baa4488a975c1cb4309d21392f02fec55
Georcelle <2234664@slu.edu.ph>
2024-04-09 01:21:16 +0800

    + fixed the microservices implementations

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/ProfileManagementChangePassAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/ProfileManagementChangePassAdminPanel.java
@@ -1,5 +1,6 @@
 package com.example.inventorymanagement.client.admin.views;
 
+import com.example.inventorymanagement.client.admin.controllers.ProfileManagementChangePassAdminController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -9,12 +10,14 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ProfileManagementChangePassAdminPanel {
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage, ProfileManagementChangePassAdminController controller) throws IOException {
 
         Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
 
         FXMLLoader fxmlLoader = new FXMLLoader(ProfileManagementChangePassAdminPanel.class.getResource("/com/example/inventorymanagement/client/view/profileManagement/profileManagementChangePass-view.fxml"));
         InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
+
+        fxmlLoader.setController(controller);
 
         if (inputStream != null) {
             Image image = new Image(inputStream);
