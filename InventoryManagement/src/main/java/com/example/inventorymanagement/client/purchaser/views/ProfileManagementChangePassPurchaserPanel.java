cd6d3efd079e1360f87462029c451309df777bb4
Georcelle <2234664@slu.edu.ph>
2024-04-08 15:29:04 +0800

    + duplicated Profile Management Change pass for purchaser user

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/views/ProfileManagementChangePassPurchaserPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/views/ProfileManagementChangePassPurchaserPanel.java
@@ -1,14 +1,34 @@
 package com.example.inventorymanagement.client.purchaser.views;
 
-import com.example.inventorymanagement.util.ClientCallback;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.rmi.registry.Registry;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ProfileManagementChangePassPurchaserPanel {
-    public ProfileManagementChangePassPurchaserPanel(Registry registry, ClientCallback clientCallback) {
-    }
 
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+
+        Font.loadFont(getClass().getResourceAsStream("/fonts/ShareTechMono-Regular.ttf"),20);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(ProfileManagementChangePassPurchaserPanel.class.getResource("/com/example/inventorymanagement/client/view/profileManagement/profileManagementChangePass-view.fxml"));
+        InputStream inputStream = getClass().getResourceAsStream("/icons/logo.png");
+
+        if (inputStream != null) {
+            Image image = new Image(inputStream);
+            stage.getIcons().add(image);
+        } else {
+            System.err.println("Failed to load image: logo.png");
+        }
+
+        Scene sceneprofileManagement = new Scene(fxmlLoader.load(), 850, 500);
+        stage.setTitle("Stock Pilot");
+        stage.setScene(sceneprofileManagement);
+        stage.show();
+        stage.setResizable(false);
     }
 }
