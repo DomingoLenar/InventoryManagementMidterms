f2c6c378d976c24161d0547f1729ad34b2a216f5
Georcelle <2234664@slu.edu.ph>
2024-04-10 01:34:18 +0800

    + added the fetchListOfUsers microservice

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/models/ProfileManagementChangePassPurchaserModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/purchaser/models/ProfileManagementChangePassPurchaserModel.java
@@ -1,6 +1,7 @@
 package com.example.inventorymanagement.client.purchaser.models;
 
 import com.example.inventorymanagement.client.microservices.ChangePasswordService;
+import com.example.inventorymanagement.client.microservices.FetchListOfUsersService;
 import com.example.inventorymanagement.util.ClientCallback;
 import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
 import com.example.inventorymanagement.util.exceptions.OutOfRoleException;
@@ -8,9 +9,11 @@ import com.example.inventorymanagement.util.exceptions.UserExistenceException;
 import com.example.inventorymanagement.util.objects.User;
 
 import java.rmi.registry.Registry;
+import java.util.LinkedList;
 
 public class ProfileManagementChangePassPurchaserModel {
     private ChangePasswordService changePasswordService;
+    private FetchListOfUsersService fetchListOfUsersService;
     private Registry registry;
     private ClientCallback clientCallback;
 
@@ -27,6 +30,15 @@ public class ProfileManagementChangePassPurchaserModel {
             // Handle any runtime exceptions
             e.printStackTrace();
             return false;
+        }
+    }
+    public LinkedList<User> fetchListOfUsers() throws NotLoggedInException, OutOfRoleException {
+        try {
+            return fetchListOfUsersService.process(clientCallback, registry);
+        } catch (RuntimeException e) {
+            // Handle any runtime exceptions
+            e.printStackTrace();
+            return null;
         }
     }
 }
