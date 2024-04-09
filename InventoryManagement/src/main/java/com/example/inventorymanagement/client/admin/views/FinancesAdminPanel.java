1fdd16f2ac9dd9afa7f453e0b863a098edb878af
Georcelle <2234664@slu.edu.ph>
2024-04-09 23:31:39 +0800

    + removed unnecessary import methods

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/FinancesAdminPanel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/views/FinancesAdminPanel.java
@@ -1,17 +1,7 @@
 package com.example.inventorymanagement.client.admin.views;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import com.example.inventorymanagement.client.admin.controllers.NavigationBarAdminController;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class FinancesAdminPanel extends Application{
     @Override
