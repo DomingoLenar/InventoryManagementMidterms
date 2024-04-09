e569e17043134415436bc0efef7b5b0f25e233fc
Georcelle <2234664@slu.edu.ph>
2024-04-10 01:33:40 +0800

    + added the fetchListOfUsers microservice

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/ProfileManagementChangePassAdminModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/admin/models/ProfileManagementChangePassAdminModel.java
@@ -1,6 +1,7 @@
 package com.example.inventorymanagement.client.admin.models;
 
 import com.example.inventorymanagement.client.microservices.ChangePasswordService;
+import com.example.inventorymanagement.client.microservices.FetchListOfUsersService;
 import com.example.inventorymanagement.util.ClientCallback;
 import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
 import com.example.inventorymanagement.util.exceptions.OutOfRoleException;
@@ -8,9 +9,11 @@ import com.example.inventorymanagement.util.exceptions.UserExistenceException;
 import com.example.inventorymanagement.util.objects.User;
 
 import java.rmi.registry.Registry;
+import java.util.LinkedList;
 
 public class ProfileManagementChangePassAdminModel {
     private ChangePasswordService changePasswordService;
+    private FetchListOfUsersService fetchListOfUsersService;
     private Registry registry;
     private ClientCallback clientCallback;
 
@@ -18,6 +21,7 @@ public class ProfileManagementChangePassAdminModel {
         this.registry = registry;
         this.clientCallback = clientCallback;
         this.changePasswordService = new ChangePasswordService();
+        this.fetchListOfUsersService = new FetchListOfUsersService();
     }
 
     public boolean changePassword(User user, String newPassword) throws UserExistenceException, OutOfRoleException, NotLoggedInException {
@@ -27,6 +31,15 @@ public class ProfileManagementChangePassAdminModel {
             // Handle any runtime exceptions
             e.printStackTrace();
             return false;
+        }
+    }
+    public LinkedList<User> fetchListOfUsers() throws NotLoggedInException, OutOfRoleException {
+        try {
+            return fetchListOfUsersService.process(clientCallback, registry);
+        } catch (RuntimeException e) {
+            // Handle any runtime exceptions
+            e.printStackTrace();
+            return null;
         }
     }
 }
