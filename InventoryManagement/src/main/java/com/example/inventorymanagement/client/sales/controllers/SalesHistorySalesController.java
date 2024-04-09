a4a51c7b814da3d9c4de904caa86878a09834702
Lenar <2233894@slu.edu.ph>
2024-04-10 05:23:47 +0800

    + Sales History Implementation of Model and Controller (Sales)

--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/SalesHistorySalesController.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/controllers/SalesHistorySalesController.java
@@ -1,45 +1,60 @@
 package com.example.inventorymanagement.client.sales.controllers;
 
-import com.example.inventorymanagement.client.common.controllers.MainController;
 import com.example.inventorymanagement.client.sales.models.SalesHistorySalesModel;
-import com.example.inventorymanagement.client.sales.views.SalesHistorySalesPanel;
+import com.example.inventorymanagement.client.common.controllers.MainController;
+import com.example.inventorymanagement.client.microservices.UpdateCallback;
 import com.example.inventorymanagement.util.ClientCallback;
 import com.example.inventorymanagement.util.ControllerInterface;
 import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
 import com.example.inventorymanagement.util.objects.Item;
 import com.example.inventorymanagement.util.objects.ItemOrder;
+import com.example.inventorymanagement.util.objects.OrderDetail;
+import com.example.inventorymanagement.util.objects.Stock;
 import com.example.inventorymanagement.util.requests.ItemOrderRequestInterface;
 import com.example.inventorymanagement.util.requests.ItemRequestInterface;
 import com.example.inventorymanagement.util.requests.UserRequestInterface;
-import javafx.application.Application;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.util.LinkedList;
 
-public class SalesHistorySalesController extends Application implements ControllerInterface {
+public class SalesHistorySalesController implements ControllerInterface {
     @FXML
-    private BorderPane borderPaneStockControlSales;
+    private BorderPane borderPaneSalesHistorySales;
     @FXML
     private Button createSalesInvoiceSalesButton;
     @FXML
     private TextField searchFieldSales;
     @FXML
     private TableView salesHistorySalesTable;
+    @FXML
+    private TableColumn<ItemOrder, String> productColumn;
+    @FXML
+    private TableColumn<ItemOrder, Integer> quantityColumn;
+    @FXML
+    private TableColumn<ItemOrder, Float> priceColumn;
+    @FXML
+    private TableColumn<ItemOrder, Float> costColumn;
+    @FXML
+    private TableColumn<ItemOrder, String> supplierColumn;
+    @FXML
+    private TableColumn<ItemOrder, String> dateColumn;
 
     @FXML
-    public BorderPane getBorderPaneStockControlSales() {
-        return borderPaneStockControlSales;
+    public BorderPane getBorderPaneSalesHistorySales() {
+        return borderPaneSalesHistorySales;
     }
 
     @FXML
-    public Button getcreateSalesInvoiceSalesButton() {
+    public Button getcreateSalesInvoiceSalesSalesButton() {
         return createSalesInvoiceSalesButton;
     }
 
@@ -53,25 +68,122 @@ public class SalesHistorySalesController extends Application implements Controll
         return salesHistorySalesTable;
     }
 
-    private SalesHistorySalesModel salesHistorySalesModel;
-    private SalesHistorySalesPanel salesHistorySalesPanel = new SalesHistorySalesPanel();
-
-    private MainController mainController;
-    private ClientCallback clientCallback;
-    private Registry registry;
-    private ItemOrder salesInvoice;
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        salesHistorySalesPanel.start(stage);
+    public TableColumn getProductColumn() {
+        return productColumn;
     }
 
-    @FXML
-    private void initialize() {
-        if (createSalesInvoiceSalesButton != null) {
-            addHoverEffect(createSalesInvoiceSalesButton);
-            createSalesInvoiceSalesButton.setOnAction(event -> handleSalesInvoice());
+    public TableColumn getQuantityColumn() {
+        return quantityColumn;
+    }
+
+    public TableColumn getPriceColumn() {
+        return priceColumn;
+    }
+
+    public TableColumn getCostColumn() {
+        return costColumn;
+    }
+
+    public TableColumn getSupplierColumn() {
+        return supplierColumn;
+    }
+
+    public TableColumn getDateColumn() {
+        return dateColumn;
+    }
+
+    private MainController mainController;
+    private SalesHistorySalesModel salesHistorySalesModel;
+    public SalesHistorySalesController() {
+
+    }
+
+    public SalesHistorySalesController(ClientCallback clientCallback, UserRequestInterface userService, ItemOrderRequestInterface iOService, ItemRequestInterface itemService, Registry registry, MainController mainController) {
+        this.salesHistorySalesModel = new SalesHistorySalesModel(registry, clientCallback);
+    }
+
+    boolean initialized = false;
+
+    @Override
+    public void fetchAndUpdate() throws RemoteException {
+        try {
+            LinkedList<ItemOrder> itemOrders = salesHistorySalesModel.fetchItems();
+            populateTableView(itemOrders);
+        } catch (NotLoggedInException e) {
+            showAlert("Error occurred while fetching items: " + e.getMessage());
         }
+    }
+
+    private void populateTableView(LinkedList<ItemOrder> itemOrders) {
+        ObservableList<ItemOrder> observableItems = FXCollections.observableArrayList(itemOrders);
+        salesHistorySalesTable.setItems(observableItems);
+
+        productColumn.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getOrderDetails().get(0).getItemId())));
+        quantityColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getOrderDetails().get(0).getQty()).asObject());
+        priceColumn.setCellValueFactory(cellData -> new SimpleFloatProperty(cellData.getValue().getOrderDetails().get(0).getUnitPrice()).asObject());
+        costColumn.setCellValueFactory(cellData -> {
+            ItemOrder order = cellData.getValue();
+            float totalCost = order.getOrderDetails().stream()
+                    .map(detail -> detail.getQty() * detail.getUnitPrice())
+                    .reduce(0.0f, Float::sum);
+            return new SimpleFloatProperty(totalCost).asObject();
+        });
+        supplierColumn.setCellValueFactory(cellData -> {
+            OrderDetail orderDetail = cellData.getValue().getOrderDetails().get(0);
+            String batchNo = orderDetail.getBatchNo();
+            Stock stock = findStockByBatchNo(batchNo);
+            return new SimpleStringProperty(stock != null ? stock.getSupplier() : "Supplier Not Found");
+        });
+        dateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDate()));
+    }
+
+    private Stock findStockByBatchNo(String batchNo) {
+        try {
+            LinkedList<ItemOrder> itemOrders = salesHistorySalesModel.fetchItems();
+            for (ItemOrder itemOrder : itemOrders) {
+                for (OrderDetail orderDetail : itemOrder.getOrderDetails()) {
+                    if (orderDetail.getBatchNo().equals(batchNo)) {
+                        return findStockByBatchNoInItemOrder(batchNo, itemOrder);
+                    }
+                }
+            }
+        } catch (NotLoggedInException e) {
+            showAlert("User is not logged in");
+        }
+        return null;
+    }
+
+    private Stock findStockByBatchNoInItemOrder(String batchNo, ItemOrder itemOrder) {
+        try {
+            for (OrderDetail orderDetail : itemOrder.getOrderDetails()) {
+                if (orderDetail.getBatchNo().equals(batchNo)) {
+                    // Assuming the batch number uniquely identifies a stock
+                    return new Stock(batchNo, orderDetail.getQty(), orderDetail.getUnitPrice(), 0, "", ""); // Create a temporary stock with available information
+                }
+            }
+        } catch (Exception e) {
+            // Handle any other exceptions
+            showAlert("Error finding stock by batch number in item order: " + e.getMessage());
+        }
+        return null; // Return null if stock with batchNo is not found or if an exception occurs
+    }
+
+    @Override
+    public String getObjectsUsed() throws RemoteException {
+        return "ItemOrder";
+    }
+
+    private void addHoverEffect(Button button) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+    }
+
+    private void showAlert(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     @FXML
@@ -79,40 +191,43 @@ public class SalesHistorySalesController extends Application implements Controll
         // Handle the event when the sales invoice button is clicked
     }
 
-    @Override
-    public void fetchAndUpdate() throws RemoteException {
-        try {
-            LinkedList<Item> items = salesHistorySalesModel.fetchItems();
-            populateTableView(items);
-        } catch (NotLoggedInException e) {
-            e.printStackTrace();
+    @FXML
+    public void initialize() { // initialize components -> better approach is to initialize just the components and let nav___bar buttons handle the population of data/realtime
+        addHoverEffect(createSalesInvoiceSalesButton);
+
+        createSalesInvoiceSalesButton.setOnAction(event -> handleSalesInvoice());
+        salesHistorySalesModel = new SalesHistorySalesModel(MainController.registry, MainController.clientCallback);
+        if (!initialized) { // Check if already initialized
+            initialized = true; // Set the flag to true
+
+            // Check if UI components are not null
+            if (salesHistorySalesTable != null && createSalesInvoiceSalesButton != null) {
+                addHoverEffect(createSalesInvoiceSalesButton);
+                createSalesInvoiceSalesButton.setOnAction(event -> handleSalesInvoice());
+
+                try {
+                    if (salesHistorySalesModel != null) {
+                        populateTableView(salesHistorySalesModel.fetchItems());
+                    } else {
+                        // Handle the case where salesHistorySalesModel is null
+                        System.out.println("Sales History Sales Model is null.");
+                    }
+                } catch (NotLoggedInException e) {
+                    // Show prompt to user not logged in
+                    System.out.println("User is not logged in.");
+                }
+            } else {
+                // Handle the case where UI components are null
+                System.out.println("Error: Table or button is null. Cannot initialize.");
+            }
         }
-    }
-
-    private void populateTableView(LinkedList<Item> items) {
-        salesHistorySalesTable.getItems().clear();
-        salesHistorySalesTable.getItems().addAll(items);
-    }
-
-    public SalesHistorySalesController() {
-
-    }
-
-    public void setMainController(MainController mainController) {
-        this.mainController = mainController;
-    }
-
-    public SalesHistorySalesController(ClientCallback clientCallback, UserRequestInterface userService, ItemOrderRequestInterface iOService, ItemRequestInterface itemService, Registry registry) {
-        SalesHistorySalesModel salesHistorySalesModel = new SalesHistorySalesModel(registry, clientCallback); // use this on events of saleshistorysalesview
-    }
-
-    @Override
-    public String getObjectsUsed() throws RemoteException {
-        return null;
-    }
-
-    private void addHoverEffect(Button button) {
-        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: derive(#EAD7D7, -10%);"));
-        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #EAD7D7;"));
+        try {
+            MainController.clientCallback.setCurrentPanel(this);
+            UpdateCallback.process(MainController.clientCallback, MainController.registry);
+        } catch (NotLoggedInException e){
+            showAlert("User is not logged in");
+        } catch (RemoteException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/models/SalesHistorySalesModel.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/client/sales/models/SalesHistorySalesModel.java
@@ -1,35 +1,34 @@
 package com.example.inventorymanagement.client.sales.models;
 
-import com.example.inventorymanagement.client.microservices.FetchListOfItemsService;
+import com.example.inventorymanagement.client.microservices.FetchSalesInvoicesService;
 import com.example.inventorymanagement.util.ClientCallback;
 import com.example.inventorymanagement.util.exceptions.NotLoggedInException;
-import com.example.inventorymanagement.util.objects.Item;
+import com.example.inventorymanagement.util.exceptions.OutOfRoleException;
+import com.example.inventorymanagement.util.objects.ItemOrder;
 
 import java.rmi.registry.Registry;
 import java.util.LinkedList;
 
 public class SalesHistorySalesModel {
-    private FetchListOfItemsService fetchLisOfItems;
+    private FetchSalesInvoicesService fetchSalesInvoicesService;
     private Registry registry;
     private ClientCallback callback;
 
 
     public SalesHistorySalesModel(Registry registry, ClientCallback clientCallback) {
-        this.fetchLisOfItems = new FetchListOfItemsService();
+        this.fetchSalesInvoicesService = new FetchSalesInvoicesService();
         this.registry = registry;
         this.callback = clientCallback;
-
     }
-    public LinkedList<Item> fetchItems () throws NotLoggedInException {
+    public LinkedList<ItemOrder> fetchItems () throws NotLoggedInException {
         try {
             // Fetch items using FetchListOfItemsService
-            return fetchLisOfItems.process(registry, callback);
-        } catch (RuntimeException e) {
+            return fetchSalesInvoicesService.process(registry, callback);
+        } catch (RuntimeException | OutOfRoleException e) {
             // Handle exceptions appropriately
             e.printStackTrace();
             return new LinkedList<>(); // Or throw an exception
         }
     }
-
 }
 
